31b743d2df70b4aad7fce76fc6a58873ba00f797
Rizky Alfiansyah <rizkywingrove@gmail.com>
2018-05-09 09:59:33 +0700

    test

---
+++ lat_starsIncrement.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 0; i < 6; i++ {
+		for j := 0; j < i; j++ {
+			if i == 4 {
+				break
+			}
+
+			//inikomendihapus
+			fmt.Print("*", " ")
+		}
+
+		if i != 4 {
+			fmt.Println()
+		}
+
+	}
+}
